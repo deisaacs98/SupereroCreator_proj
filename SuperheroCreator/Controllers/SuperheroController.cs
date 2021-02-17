626af0a0199c824665eff6d139776b65da31b3b3
Dan Isaacs <deisaacs98@gmail.com>
2021-02-17 12:13:46 -0600

    Added views and controller.

---
+++ SuperheroCreator/Controllers/SuperheroController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SuperheroCreator.Data;
+using SuperheroCreator.Models;
+
+namespace SuperheroCreator.Controllers
+{
+    public class SuperheroController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public SuperheroController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var superheroes = _context.Superheroes;
+            return View(superheroes);
+        }
+
+        //HttpGet
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //HttpPost
+        [HttpPost]
+        public IActionResult Create(Superhero superhero)
+        {
+            _context.Superheroes.Add(superhero);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
